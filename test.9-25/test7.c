57c54a9636d38309a8a204e74ca85c832bd32693
荒域 <3362945936@qq.com>
2023-09-26 20:15:39 +0800

    提交代码

--- test.9-25/test7.c
+++ test.9-25/test7.c
@@ -1,31 +1,47 @@
-#include <stdio.h>
+// #include <stdio.h>
+
+// int main ()
+// {   
+//     int day =0;
+//     scanf("%d",&day);
+//     switch(day)
+//     {
+//         case 1:
+//         case 2:
+//         case 3:
+//         case 4:
+//         case 5:
+//         printf("工作日\n");
+//         break;
+//         case 6:
+//         case 7:
+//         printf("休息日\n");
+//         break;
+
+
+
+
+//     }
+
+
+
+
+
+//     return 0;
+// }
+
+#include <stdio.h>//小飞机for 循环、
 
 int main ()
-{   
-    int day =0;
-    scanf("%d",&day);
-    switch(day)
-    {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        printf("工作日\n");
-        break;
-        case 6:
-        case 7:
-        printf("休息日\n");
-        break;
-
-
-
-
-    }
-
-
-
-
+{
+    for (int i=0;i<2;i++)
+    printf("     **\n");
+    for (int i=2;i>=2&&i<4;i++)
+    printf("************\n");
+    for (int i =4;i>=4&&i<6;i++)
+    printf("    *  *\n");
+    
+    
 
     return 0;
 }
