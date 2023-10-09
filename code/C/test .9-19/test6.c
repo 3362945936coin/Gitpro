c48d5af04c242d71ed2a7165fbf0c29e112d7049
3362945936coin <3362945936@qq.com>
2023-10-09 21:45:08 +0800

    战！

--- code/C/test .9-19/test6.c
+++ code/C/test .9-19/test6.c
@@ -1,20 +1,57 @@
-#include <stdio.h>
-
-int main() {
-    printf("\n\t\t\t      @@@              @@@");
-    printf("\n\t\t\t   @@@@@@@@        @@@@@@@@");
-    printf("\n\t\t\t@@@@@@@@@@@      @@@@@@@@@@@");
-    printf("\n\t\t\t@@@@@@@@@@@@    @@@@@@@@@@@@@");
-    printf("\n\t\t\t@@@@@@@@@@@@@  @@@@@@@@@@@@@@");
-    printf("\n\t\t\t @@@@@@@@@@@@@@@@@@@@@@@@@@@");
-    printf("\n\t\t\t   @@@@@@@@@@@@@@@@@@@@@@@");
-    printf("\n\t\t\t     @@@@@@@@@@@@@@@@@@@");
-    printf("\n\t\t\t       @@@@@@@@@@@@@@@");
-    printf("\n\t\t\t         @@@@@@@@@@@");
-    printf("\n\t\t\t           @@@@@@@");
-    printf("\n\t\t\t             @@@");
-    printf("\n\t\t\t              @\n\n");
-
-    return 0;
+// 
+// #include<stdio.h>//写法二，
+// int main()
+// {
+// 	char s = 0;
+// 	int i = 0, k = 0;
+// 	scanf("%d%c%d", &i,&s,&k);
+// 	switch (k)
+// 	{
+// 	case 1:
+// 	case 3:
+// 	case 5:
+// 	case 7:
+// 	case 9:
+// 	case 11:
+// 		printf("31\n");
+// 		break;
+// 	case 4:
+// 	case 6:
+// 	case 8:
+// 	case 10:
+// 	case 12:
+// 		printf("30\n");
+// 		break; 
+// 	case 2:
+// 		if ((i % 4 == 0 && i % 100 != 0) || i % 400 == 0)
+// 		{
+// 			printf("29\n");
+// 		}
+// 		else
+// 		{
+// 			printf("28\n");
+// 		}
+// 		break;
+// 	}
+// 	return 0;
+// }
+ 
+ #include <stdio.h>//直角三角形判定
+ int main ()
+ {
+ int a,b,c,d,e,f;
+ scanf("%d,%d,%d",&a,&b,&c);
+ if(a+b>c&&a+c>b&&b+c>a)//在三角形中，如果任意两边的平方和等于第三边平方和，那一定是∟三角形。即逆定理。并且满足任意两边之和，，，
+{
+  ;
 }
-
+if(a*a+b*b==c*c||a*a+c*c==b*b||b*b+c*c==a*a) 
+{
+  printf("%d,%d,%d:Y",a,b,c);
+}
+else
+{
+  printf("%d,%d,%d:N",a,b,c);
+}
+  return 0;
+ }
--- test.10-4/test10.c
+++ test.10-4/test10.c
@@ -119,29 +119,30 @@
 //   return 0;
 
 // }
-#include <stdio.h>//中间值
-int main()
-{
-  int a,b,c,mid;
-  scanf("%d %d %d",&a,&b,&c);
-  if(a>b)
-  {
-   mid=a;
-   a=b;
-   b=mid;
-  }
-  if(a>c)
-  {
-    mid =a;
-    a=c;
-    c=mid;
-  }
-  if(b>c)
-  {
-    mid=b;
-    b=c;
-    c=mid;
-  } 
-printf("%d",b);
-  return 0;
-}
+// #include <stdio.h>//中间值
+// int main()
+// {
+//   int a,b,c,mid;
+//   scanf("%d %d %d",&a,&b,&c);
+//   if(a>b)//交换
+//   {
+//    mid=a;
+//    a=b;
+//    b=mid;
+//   }
+//   if(a>c)
+//   {
+//     mid =a;
+//     a=c;
+//     c=mid;
+//   }
+//   if(b>c)
+//   {
+//     mid=b;
+//     b=c;
+//     c=mid;
+//   } 
+// printf("%d",b);
+//   return 0;
+// }
+
